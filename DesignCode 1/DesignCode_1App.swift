70ad8e4fd60f4641ed2fe8573fff62f3c8d00f2d
C1oud03 <c1oud03@yandex.ru>
2021-03-01 20:36:07 +0400

    Initial Commit

---
+++ DesignCode 1/DesignCode_1App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCode_1App.swift
+//  DesignCode 1
+//
+//  Created by Семён Рытенков on 01.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct DesignCode_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
