998de1d64f3ebb97033ca3242e8bdbb89ee7e87e
C1oud03 <c1oud03@yandex.ru>
2021-03-03 11:28:34 +0400

    added button and modal presentation

--- DesignCode 1/View/HomeView.swift
+++ DesignCode 1/View/HomeView.swift
@@ -33,6 +33,7 @@ let sectionData = [
 
 struct HomeView: View {
     @Binding var showProfile: Bool
+    @State var showUpdate = false
     
     var body: some View {
         VStack {
@@ -43,6 +44,22 @@ struct HomeView: View {
                 Spacer()
                 
                 AvatarView(showProfile: $showProfile)
+                
+                Button(action: {
+                    showUpdate.toggle()
+                }) {
+                    Image(systemName: "bell")
+                        .renderingMode(.original)
+                        .font(.system(size: 16, weight: .medium))
+                        .frame(width: 36, height: 36)
+                        .background(Color.white)
+                        .clipShape(Circle())
+                        .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
+                }
+                .sheet(isPresented: $showUpdate) {
+                    ContentView()
+                }
             } //: HSTACK
             .padding(.horizontal)
             .padding(.leading, 14)
