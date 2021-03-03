33d99e10bfcff6292f1090c5614dbec7172bc0ad
C1oud03 <c1oud03@yandex.ru>
2021-03-03 13:35:22 +0400

    added combine and buttons: add, edit for UpdateList

--- DesignCode 1/View/HomeView.swift
+++ DesignCode 1/View/HomeView.swift
@@ -58,7 +58,7 @@ struct HomeView: View {
                         .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
                 }
                 .sheet(isPresented: $showUpdate) {
-                    ContentView()
+                    UpdateList()
                 }
             } //: HSTACK
             .padding(.horizontal)
