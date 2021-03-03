11ebfa90d333717cecbfdf0a0c09d1dd86bbcaa7
C1oud03 <c1oud03@yandex.ru>
2021-03-03 09:46:39 +0400

    added binding, screen size,  drag  progress and tap  background

--- DesignCode 1/View/HomeView.swift
+++ DesignCode 1/View/HomeView.swift
@@ -2,59 +2,34 @@
 //  HomeView.swift
 //  DesignCode 1
 //
-//  Created by Семён Рытенков on 02.03.2021.
+//  Created by Семён Рытенков on 03.03.2021.
 //
 
 import SwiftUI
 
 struct HomeView: View {
-    @State var showProfile = false
+    @Binding var showProfile: Bool
     
     var body: some View {
-        ZStack {
-            Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack {
-                HStack {
-                    Text("Watching")
-                        .font(.system(size: 28, weight: .bold, design: .rounded))
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        showProfile.toggle()
-                    }) {
-                        Image("Avatar")
-                            .resizable()
-                            .frame(width: 36, height: 36)
-                            .clipShape(Circle())
-                    } //: BUTTON
-                } //: HSTACK
-                .padding(.horizontal)
-                .padding(.top, 30)
+        VStack {
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28, weight: .bold, design: .rounded))
                 
                 Spacer()
-            } //: VSTACK
-            .padding(.top, 44)
-            .background(Color.white)
-            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
-            .offset(y: showProfile ? -450 : 0)
-            .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0), axis: (x: 10.0, y: 0.0, z: 0.0))
-            .scaleEffect(showProfile ? 0.9 : 1)
-            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-            .edgesIgnoringSafeArea(.all)
+                
+                AvatarView(showProfile: $showProfile)
+            } //: HSTACK
+            .padding(.horizontal)
+            .padding(.top, 30)
             
-            MenuView()
-                .offset(y: showProfile ? 0 : 600)
-                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-        } //: ZSTACK
+            Spacer()
+        } //: VSTACK
     } //: BODY
 }
 
-struct HomeView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {    
     static var previews: some View {
-        HomeView()
+        HomeView(showProfile: .constant(false))
     }
 }
